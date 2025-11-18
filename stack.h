43e636588283202b9f8153c349d441ca95f4d08b
aekasit-max <aekasit.pun@student.mahidol.edu>
2025-11-18 04:02:03 +0000

    Updated

--- stack.h
+++ stack.h
@@ -2,6 +2,9 @@
 #ifndef stack_h
 #define stack_h
 #include "node.h"
+#include <iostream>
+
+using namespace std;
 class Stack {
 	NodePtr top;
 	int size;
@@ -17,6 +20,9 @@ void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
             // Left missing for exercises…
+            new_node -> set_next(top);
+            top = new_node;
+            size++;
    }
  
          // Left missing for exercises…
@@ -28,17 +34,28 @@ int Stack::pop(){
         int value;
         value=t->get_value();
     // Left missing part for exercises
+        --size;
         delete t;
         return value;
 	//be careful of the empty stack!!!
-    }
+    
 
+    cout<<"Empty stack"<<endl;
+    return 0;
+}
 Stack::Stack(){
     //initialize stack
+    top = NULL;
+    size = 0;
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
+    cout<<"Clear the stack"<<endl;
+    int n = size;
+    while(n>0)
+    pop();
+    n--;
     
 }
 
